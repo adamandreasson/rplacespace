3376ca5cb0675027b22a705e778be22acdbec518
Adam Andreasson <hej@adamandreasson.se>
2022-04-02 16:45:48 +0200

    update graphic

--- spaceflight.user.js
+++ spaceflight.user.js
@@ -12,16 +12,24 @@
 // ==/UserScript==
 
 if (window.top !== window.self) {
-    window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
-            const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/182512780425822209/959820838356529152/trans_space_logo_v7.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
-            console.log(i);
-            return i;
-        })())
-
-    }, false);
-
+	window.addEventListener(
+		"load",
+		() => {
+			document
+				.getElementsByTagName("mona-lisa-embed")[0]
+				.shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0]
+				.shadowRoot.children[0].appendChild(
+					(function () {
+						const i = document.createElement("img");
+						i.src =
+							"https://cdn.discordapp.com/attachments/182512780425822209/959825803791720488/trans_space_logo_v8.png";
+						i.style =
+							"position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+						console.log(i);
+						return i;
+					})()
+				);
+		},
+		false
+	);
 }
